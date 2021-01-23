2136b5d862e8329388108738aab9605608e1737f
fanshc <51053655+fanshc@users.noreply.github.com>
2021-01-24 00:52:47 +0800

    Add files via upload
    第二周作业提交

---
+++ week-2/星期日作业/第二题/src/main/java/com/geetbang/homework04/server/HttpServerBySocket.java
@@ -0,0 +1,37 @@
+package com.geetbang.homework04.server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 通过Socket编程实现http服务器
+ */
+public class HttpServerBySocket {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            serviceSocket(socket);
+        }
+
+    }
+
+    private static void serviceSocket(Socket socket) {
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello,nio1";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
